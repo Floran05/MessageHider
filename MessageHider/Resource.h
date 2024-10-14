50e9c096536770ab76ba07fe878836efb53ea623
bfaverot <bfaverot@GCFRLY008>
2024-10-14 12:24:36 +0200

    push des bouton sans logic

--- MessageHider/Resource.h
+++ MessageHider/Resource.h
@@ -12,6 +12,11 @@
 #define IDI_SMALL                       108
 #define IDC_MESSAGEHIDER                109
 #define IDR_MAINFRAME                   128
+#define ID_FICHIER_CRYPTERUNEIMAGE      32771
+#define ID_FICHIER_DECRYPTERUNEIMAGE    32772
+#define ID_AIDE_LACHANSONOFFICIELLE     32773
+#define ID_APPLICATION_PARAMETRES       32774
+#define ID_Menu                         32775
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +25,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
