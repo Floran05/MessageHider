43cf1502d0125f65291a2f6404744f43721f15d9
fcarvalho <fcarvalho@gaming.tech>
2024-10-17 13:04:55 +0200

    F5Stegano

--- MessageHider/F5Stegano.cpp
+++ MessageHider/F5Stegano.cpp
@@ -1,9 +1,40 @@
 #include "F5Stegano.h"
 
-std::vector<std::vector<unsigned char>> F5Stegano::GetBlock(int index)
+F5Stegano::F5Stegano()
 {
-    //std::vector<int> Test(64, 10)
+}
 
+F5Stegano::~F5Stegano()
+{
+}
 
-    //return std::vector<std::vector<unsigned char>>();
+std::vector<std::vector<unsigned char>> F5Stegano::GetBlock(unsigned char* pixel_tab, const size_t len_pixel_tab, int nb_bytes_per_pixel, int index)
+{
+    /*std::vector<std::vector<unsigned char>> out(8 * nb_bytes_per_pixel, std::vector<unsigned char>(8 * nb_bytes_per_pixel));
+
+    int block = 1;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            int start = (block * nb_bytes_per_pixel * i) + ()
+        }
+    }*/
+
+    return std::vector<std::vector<unsigned char>>();
+}
+
+std::vector<std::vector<float>> F5Stegano::ApplyDCT(const std::vector<std::vector<unsigned char>>& block)
+{
+    return std::vector<std::vector<float>>();
+}
+
+std::vector<std::vector<int>> F5Stegano::QuantizeDCT(const std::vector<std::vector<float>>& dctCoeffs)
+{
+    return std::vector<std::vector<int>>();
+}
+
+void F5Stegano::Encrypt(unsigned char* pixel_tab, const size_t len_pixel_tab, int nb_bytes_per_pixel, std::string word_to_hide)
+{
+
 }
--- MessageHider/F5Stegano.h
+++ MessageHider/F5Stegano.h
@@ -4,10 +4,22 @@
 
 class F5Stegano : public AlgoStegano
 {
-
 public:
 
-	std::vector<std::vector<unsigned char>> GetBlock(int index);
+	F5Stegano();
+	virtual ~F5Stegano();
+
+protected:
+
+protected:
+
+	std::vector<std::vector<unsigned char>> GetBlock(unsigned char* pixel_tab, const size_t len_pixel_tab, int nb_bytes_per_pixel, int index);
+	std::vector<std::vector<float>> ApplyDCT(const std::vector<std::vector<unsigned char>>& block);
+	std::vector<std::vector<int>> QuantizeDCT(const std::vector<std::vector<float>>& dctCoeffs);
+
+public:
+	void Encrypt(unsigned char* pixel_tab, const size_t len_pixel_tab, int nb_bytes_per_pixel, std::string word_to_hide) override;
+
 
 };
 
